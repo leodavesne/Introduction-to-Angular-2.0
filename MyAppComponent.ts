59c5e9fdf008d2f80482f7bc492cbf768b655851
Léo Davesne <leo@davesne.net>
2016-08-03 18:24:44 +0200

    4 Building Blocks of Angular 2.0 - Introducing Components

---
+++ MyAppComponent.ts
@@ -0,0 +1,8 @@
+@Component({
+	selector: 'my-app',
+	template: `<h3>Task List Application</h3>`,
+	styles: ['h3 { color: gray; }']
+})
+export class MyAppComponent {
+	console.Log("Hello Angular");
+}
---
+++ MyAppComponent02.ts
@@ -0,0 +1,10 @@
+@Component({
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css'],
+	directives: [],
+	providers: []
+})
+export class MyAppComponent {
+	console.Log("Hello Angular");
+}
