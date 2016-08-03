5c99a0823b6dc964142f245d08e107ffb8ef238a
Léo Davesne <leo@davesne.net>
2016-08-03 18:00:07 +0200

    2 Introduction to TypeScript

---
+++ TypeScriptExample.ts
@@ -0,0 +1,16 @@
+var num: number = 5;
+var name: string = "Speros";
+var something: any = 123;
+var list: Array<number> = [1, 2, 3];
+
+function square(num: number): number {
+	return num * num;
+}
+
+class Person {
+	constructor(public name: string) {
+
+	}
+}
+
+var aPerson = new Person("Ada Lovelace");
---
+++ TypeScriptExample02.ts
@@ -0,0 +1,18 @@
+var name: string = "Léo";
+var num: number = 15;
+var myArr: Array<number> = [1, 2, 3];
+
+var something: any = 15;
+something = "hello";
+
+function sample(first: string): string {
+	return first;
+}
+
+class Person {
+	constructor(public name: string) {
+
+	}
+}
+
+var aPerson: Person = new Person("PersonName");
