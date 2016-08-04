c14e6cfc24202734076ad9d981d1b0002cd8aa04
Léo Davesne <leo@davesne.net>
2016-08-04 10:03:53 +0200

    Creating Our First Component

---
+++ app/main.js
@@ -0,0 +1,43 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var core_1 = require('@angular/core');
+var TasksComponent = (function () {
+    function TasksComponent() {
+    }
+    TasksComponent.prototype.ngOnInit = function () { };
+    TasksComponent = __decorate([
+        core_1.Component({
+            selector: 'tasks',
+            template: "\n\t<h4>This is the Tasks Component</h4>\n\t"
+        }), 
+        __metadata('design:paramtypes', [])
+    ], TasksComponent);
+    return TasksComponent;
+}());
+exports.TasksComponent = TasksComponent;
+var AppComponent = (function () {
+    function AppComponent() {
+    }
+    AppComponent.prototype.ngOnInit = function () { };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            directives: [TasksComponent],
+            template: "\n\t<h1>Hello World!</h1>\n\t<tasks></tasks>\n\t"
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+platform_browser_dynamic_1.bootstrap(AppComponent);
+//# sourceMappingURL=main.js.map
